b2dfd65d45fc00e4ab36f3bd3671db4f087528f1
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-10-25 11:23:40 -0300

    calendario

---
+++ src/views/ClassesView.tsx
@@ -0,0 +1,11 @@
+import Calendario from "@/components/Calendar"
+
+const ClasesView = () => {
+    return(
+        <div className="w-4xl h-10 bg-(--secondary)">
+            <h2 className="text-(--foreground) text-3xl font-bold">Clendario</h2>    
+        </div>
+    )
+}
+
+export default ClasesView
