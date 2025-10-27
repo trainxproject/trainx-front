8eb41681998ce479c329bcb78ae0b8d1f7b5c562
Agustina Armendariz <armendarizagustina.m@gmail.com>
2025-10-26 21:43:04 -0300

    logo, card suscripciones

---
+++ src/mocks/quickStats.ts
@@ -0,0 +1,6 @@
+export const quickStats = [
+  { label: "Socios Activos", value: "245+" },
+  { label: "Clases Semanales", value: "80+" },
+  { label: "Entrenadores", value: "12" },
+  { label: "Años de Experiencia", value: "15" },
+];
