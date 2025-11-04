e2673e3bbd00c864f2aec86e52f7413df87304ab
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-11-04 16:58:49 -0300

    demo 2

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,10 +1,7 @@
 import DashboardUserView from "@/views/DashboardUserView";
-import ProtectedRoutes from "@/components/ProtectedRout"
 
 export default function DashboardUser() {
     return(
-        <ProtectedRoutes>
             <DashboardUserView />
-        </ProtectedRoutes>
     )
 }
