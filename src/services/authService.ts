cdbd0bbe6f10c5b8aecb0429c9905565df548956
lautaroalfonso423 <sebastiansosa3011@gmail.com>
2025-11-04 19:45:49 -0300

    cambios

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { IUser, IAuthResponse, ILoginData, IRegisterData } from "@/interfaces/User";
 
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 export const loginUser = async (data: ILoginData): Promise<IAuthResponse> => {
   const { data: response } = await axios.post<IAuthResponse>(`${API_URL}/auth/login`, data);
