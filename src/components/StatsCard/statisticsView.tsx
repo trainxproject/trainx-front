b0ad7435a9add0ad29df2b8fe5258f710095aa6c
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-11-13 23:57:11 -0300

    estadisticas

--- src/components/StatsCard/statisticsView.tsx
+++ src/components/StatsCard/statisticsView.tsx
@@ -1,6 +1,56 @@
 'use client'
 
-const Statistics = () => {
+import { Plan } from "@/interfaces/Plan";
+import { getThreeDayPlan, getFiveDayPlan, getMonthlyCollection } from "@/services/adminServices";
+import { fetchServerResponse } from "next/dist/client/components/router-reducer/fetch-server-response";
+import { useEffect, useState } from "react";
+
+const Statistics: React.FC =  () => {
+
+    const [threeDay, setThreeDay] = useState<Plan[]>([])
+    const [fiveDay, setFiveDay] = useState<Plan[]>([])
+    const [MonthlyCollection, setMonthlyCollection] = useState<Number>()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const threeDay = await getThreeDayPlan();
+                console.log(threeDay);
+                setThreeDay(threeDay);
+            } catch (error) {
+                console.error("Error al traer los planes de tres días: ", error);
+                return[]
+            }
+        }
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const fiveDay = await getFiveDayPlan();
+                console.log(fiveDay);
+                setFiveDay(fiveDay);
+            } catch (error) {
+                console.error("Error al traer los planes de cinco días: ", fiveDay);
+                return[]
+            }
+        }
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const recaudacion = await getMonthlyCollection();
+                console.log(recaudacion);
+                setMonthlyCollection(recaudacion);
+            } catch (error) {
+                console.error("Error al traer la recaudacion mensual: ", error);
+                return[];
+            }
+        }
+    })
 
     return(
         <div className="flex flex-col">
