c09d0fa884fcac92c2e5112df9892c5fdf098cb2
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-11-21 15:37:48 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Mail, Phone, MapPin, Pin } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Footer() {
   const currentYear = new Date().getFullYear();
@@ -62,12 +63,12 @@ export function Footer() {
               <li className="flex items-start gap-2">
                 <Pin className="w-4 h-4 mt-1 flex-shrink-0" style={{ color: "var(--primary)" }} />
                 <span className="text-[14px] text-muted-foreground">
-                  Av. Principal 123, Buenos Aires, Argentina
+                  Intendente Biscayart Sur 452
                 </span>
               </li>
               <li className="flex items-center gap-2">
                 <Phone className="w-4 h-4 flex-shrink-0" style={{ color: "var(--primary)" }} />
-                <span className="text-[14px] text-muted-foreground">+549 11 1234 5678</span>
+                <span className="text-[14px] text-muted-foreground">+549 2477 557832</span>
               </li>
               <li className="flex items-center gap-2">
                 <Mail className="w-4 h-4 flex-shrink-0" style={{ color: "var(--primary)" }} />
@@ -75,9 +76,10 @@ export function Footer() {
               </li>
               <li className="flex items-center gap-2">
                 <MapPin className="w-4 h-4 flex-shrink-0" style={{ color: "var(--primary)" }} />
-                 <a href="#" className="text-[14px] text-muted-foreground hover:text-[#FF6B00] transition-colors">
+                 <Link href="https://www.google.com/maps/place/Club+N%C3%A1utico+Pergamino/@-33.9038814,-60.5786066,705m/data=!3m1!1e3!4m6!3m5!1s0x95b9b5fdb5968f3b:0x6a50115ae9c8ec5a!8m2!3d-33.9039177!4d-60.5760481!16s%2Fg%2F11sq54yg_4?entry=ttu&g_ep=EgoyMDI1MTExNy4wIKXMDSoASAFQAw%3D%3D" className="text-[14px] text-muted-foreground hover:text-[#FF6B00] transition-colors" target="_blank"
+ rel="noopener noreferrer">
                 Mapa del Sitio
-              </a>
+              </Link>
               </li>
             </ul>
           </div>
